4a796661b1ce5989c9f42b51e89a3a0ff53bb244
Shamarvey1 <ajitlookforbest@gmail.com>
2025-05-06 02:05:00 +0530

    S

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import About from './components/About';
+import Home from './components/Home';
+import Product from './components/Product';
+import Blog from './components/Blog';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+   <nav className="navbar">
+       
+          <div className='brand'>
+          Mental Health Tracker
+          </div>
+          <div className='links'>
+          <Link to="/home" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/products" className="nav-link">Products</Link>
+          <Link to="/Blog" className="nav-link">Blog</Link>
+          </div>
+          <div className='Button'>
+            <button>Login</button>
+            <button>Sign Up</button>
+          </div>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path= "/products" element={<Product/>}/>
+        <Route path= "/Blog" element={<Blog/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
